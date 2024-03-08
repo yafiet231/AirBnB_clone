d52b819668cce8b9ac7ff04d64e97ed991bbc0dd
Mahari9 <merytseg77@gmail.com>
2024-03-08 13:04:51 +0000

    Amenity Class

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This module defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel.
+        
+      Public class attribute:
+           name:string- Name of the amenity. 
+    """
+    name = ""
